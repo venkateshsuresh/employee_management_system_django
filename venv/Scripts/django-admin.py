5c1f48aaaca5df142d2db9ebb53c7c8526a28f5d
Venkat <venkatvijay20017@gmail.com>
2020-06-23 18:32:36 +0530

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\91908\desktop\edureka\myproject\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ webapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import employees
+from . serializers import employeesSerializer
+
+def index(request):
+    return render(request, 'webapp/index.html')
+def hello(request):
+    return render(request, 'webapp/hello.html')
+class employeeList(APIView):
+
+    def get(self,request):
+        employees1= employees.objects.all()
+        serializer= employeesSerializer(employees1,many=True)
+        return Response(serializer.data)
+
+
+    def post(self):
+        pass
+
+
+
